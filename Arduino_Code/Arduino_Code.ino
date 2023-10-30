ac4880fe9b49b684b36d20709aedb8fbaf23707a
nas11ai <74126181+nas11ai@users.noreply.github.com>
2023-10-31 00:45:51 +0800

    feat: init esp32 api code

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,92 +1,148 @@
-#define LED_1 27  
-#define LED_2 12
-#define LED_3 14
-#define LED_4 26
-#define LED_5 33
-#define LED_6 15
-#define LED_7 32
-#define LED_8 16
-#define LED_9 4
-#define LED_10 2
-#define LED_11 1
-#define LED_12 13
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <map>
 
+const char* ssid = "Nama_Wifi";
+const char* password = "Password_Wifi";
+
+AsyncWebServer server(80);
+
+struct Road {
+  int hijau;
+  int kuning;
+  int merah;
+};
+
+std::map<int, Road> roads = {
+    {1, {27, 12, 14}},
+    {2, {26, 33, 15}},
+    {3, {32, 16, 4}},
+    {4, {2, 1, 13}}
+};
+
+bool normalMode = true; // Variabel untuk menandakan mode normal atau tidak
+bool blinkMode = false; // Variabel untuk menandakan mode kuning kelap-kelip
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_1, OUTPUT);
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED_3, OUTPUT);
-  pinMode(LED_4, OUTPUT);
-  pinMode(LED_5, OUTPUT);
-  pinMode(LED_6, OUTPUT);
-  pinMode(LED_7, OUTPUT);
-  pinMode(LED_8, OUTPUT);
-  pinMode(LED_9, OUTPUT);
-  pinMode(LED_10, OUTPUT);
-  pinMode(LED_11, OUTPUT);
-  pinMode(LED_12, OUTPUT);
- 
+  Serial.begin(115200);
+
+  // Inisialisasi pin LED
+  for(auto const &entry : roads) {
+    pinMode(entry.second.hijau, OUTPUT);
+    pinMode(entry.second.kuning, OUTPUT);
+    pinMode(entry.second.merah, OUTPUT);
+  }
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+
+  server.on("/normal", HTTP_GET, [](AsyncWebServerRequest *request){
+    normalMode = true; // Aktifkan mode normal
+    blinkMode = false;
+    sendJsonResponse(request, 200, "Normal traffic light sequence started.");
+  });
+
+  server.on("/green", HTTP_GET, [](AsyncWebServerRequest *request){
+    String roadId = request->getParam("road")->value();
+    int roadNumber = roadId.toInt();
+    normalMode = false; // Nonaktifkan mode normal
+    blinkMode = false;
+    if (roads.find(roadNumber) != roads.end()) {
+      greenTrafficLight(roadNumber);
+      sendJsonResponse(request, 200, "Green light activated for road " + roadId);
+    } else {
+      sendJsonResponse(request, 400, "Invalid road specified.");
+    }
+  });
+
+  server.on("/blink", HTTP_GET, [](AsyncWebServerRequest *request){
+    normalMode = false; // Nonaktifkan mode normal
+    blinkMode = true;
+    sendJsonResponse(request, 200, "Traffic light blinking sequence started.");
+  });
+
+  server.begin();
 }
 
 void loop() {
-  digitalWrite(LED_1, HIGH);
-  delay(500);
-  digitalWrite(LED_1, LOW);
-  delay(500);
-  
-  digitalWrite(LED_2, HIGH);
-  delay(500);
-  digitalWrite(LED_2, LOW);
-  delay(500);
-  
-  digitalWrite(LED_3, HIGH);
-  delay(500);
-  digitalWrite(LED_3, LOW);
-  delay(500);
+  if(normalMode) {
+    // Jika dalam mode normal, jalankan urutan lampu lalu lintas normal
+    normalTrafficLight();
+  } else if(blinkMode) {
+    // jika dalam mode blink, lampu lalu lintas menjadi kuning kelap-kelip
+    blinkTrafficLight();
+  }
+}
 
-  digitalWrite(LED_4, HIGH);
-  delay(500);
-  digitalWrite(LED_4, LOW);
-  delay(500);
-  
-  digitalWrite(LED_5, HIGH);
-  delay(500);
-  digitalWrite(LED_5, LOW);
-  delay(500);
-  
-  digitalWrite(LED_6, HIGH);
-  delay(500);
-  digitalWrite(LED_6, LOW);
-  delay(500);
+void sendJsonResponse(AsyncWebServerRequest *request, int status, String message) {
+  String response = "{\"status\": " + String(status) + ", \"message\": \"" + message + "\"}";
+  request->send(200, "application/json", response);
+}
 
-  digitalWrite(LED_7, HIGH);
-  delay(500);
-  digitalWrite(LED_7, LOW);
-  delay(500);
-  
-  digitalWrite(LED_8, HIGH);
-  delay(500);
-  digitalWrite(LED_8, LOW);
-  delay(500);
-  
-  digitalWrite(LED_9, HIGH);
-  delay(500);
-  digitalWrite(LED_9, LOW);
-  delay(500);
-  
-  digitalWrite(LED_10, HIGH);
-  delay(500);
-  digitalWrite(LED_10, LOW);
-  delay(500);
-  
-  digitalWrite(LED_11, HIGH);
-  delay(500);
-  digitalWrite(LED_11, LOW);
-  delay(500);
-  
-  digitalWrite(LED_12, HIGH);
-  delay(500);
-  digitalWrite(LED_12, LOW);
-  delay(500);
+void normalTrafficLight() {
+  for(auto const &entry : roads) {
+    // Matikan semua lampu
+    turnOffLights(entry.second);
+
+    // Nyalakan lampu merah
+    digitalWrite(entry.second.merah, HIGH);
+    delay(3000);
+
+    // Nyalakan lampu kuning
+    digitalWrite(entry.second.kuning, HIGH);
+    delay(1000);
+
+    // Nyalakan lampu hijau
+    digitalWrite(entry.second.hijau, HIGH);
+    delay(3000);
+  }
+}
+
+void greenTrafficLight(int roadNumber) {
+  Road& road = roads[roadNumber];
+
+  // Matikan lampu lalu lintas yang lain dulu
+  turnOffLights(road);
+
+  // Nyalakan lampu merah untuk jalur yang tidak dipilih
+  for(auto const &entry : roads) {
+    if (entry.first != roadNumber) {
+      digitalWrite(entry.second.merah, HIGH);
+    }
+  }
+
+  // Nyalakan lampu hijau jalan sesuai permintaan
+  digitalWrite(road.hijau, HIGH);
+  delay(5000);
+
+  // Matikan semua lampu
+  turnOffLights(road);
+
+  // Delay 1 detik
+  delay(1000);
+}
+
+void blinkTrafficLight() {
+  for(auto const &entry : roads) {
+    // Nyalakan kuning
+    digitalWrite(entry.second.kuning, HIGH);
+  }
+  delay(2000);
+
+  // Matikan semua lampu kuning
+  for(auto const &entry : roads) {
+    digitalWrite(entry.second.kuning, LOW);
+  }
+  delay(2000);
+}
+
+void turnOffLights(const Road& road) {
+  digitalWrite(road.hijau, LOW);
+  digitalWrite(road.kuning, LOW);
+  digitalWrite(road.merah, LOW);
 }
